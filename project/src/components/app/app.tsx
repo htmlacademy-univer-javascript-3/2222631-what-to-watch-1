52543b252c0ccb575c567456672a7b8b630948bc
Med-d <logvinov.maksim123@gmail.com>
2022-12-11 22:50:20 +0500

    Add routings to pages

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,100 +1,59 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {Main} from '../../pages/Main';
-import {FilmCardProps} from '../FilmCard';
-
-const filmsListProps: FilmCardProps[] = [
-  {
-    title: 'Fantastic Beasts: The Crimes of Grindelwald',
-    imgLink: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg'
-  },
-  {
-    title: 'Bohemian Rhapsody',
-    imgLink: 'img/bohemian-rhapsody.jpg'
-  },
-  {
-    title: 'Macbeth',
-    imgLink: 'img/macbeth.jpg'
-  },
-  {
-    title: 'Aviator',
-    imgLink: 'img/aviator.jpg'
-  },
-  {
-    title: 'We need to talk about Kevin',
-    imgLink: 'img/we-need-to-talk-about-kevin.jpg'
-  },
-  {
-    title: 'What We Do in the Shadows',
-    imgLink: 'img/what-we-do-in-the-shadows.jpg'
-  },
-  {
-    title: 'Revenant',
-    imgLink: 'img/revenant.jpg'
-  },
-  {
-    title: 'Johnny English',
-    imgLink: 'img/johnny-english.jpg'
-  },
-  {
-    title: 'Shutter Island',
-    imgLink: 'img/shutter-island.jpg'
-  },
-  {
-    title: 'Pulp Fiction',
-    imgLink: 'img/pulp-fiction.jpg'
-  },
-  {
-    title: 'No Country for Old Men',
-    imgLink: 'img/no-country-for-old-men.jpg'
-  },
-  {
-    title: 'Snatch',
-    imgLink: 'img/snatch.jpg'
-  },
-  {
-    title: 'Moonrise Kingdom',
-    imgLink: 'img/moonrise-kingdom.jpg'
-  },
-  {
-    title: 'Seven Years in Tibet',
-    imgLink: 'img/seven-years-in-tibet.jpg'
-  },
-  {
-    title: 'Midnight Special',
-    imgLink: 'img/midnight-special.jpg'
-  },
-  {
-    title: 'War of the Worlds',
-    imgLink: 'img/war-of-the-worlds.jpg'
-  },
-  {
-    title: 'Dardjeeling Limited',
-    imgLink: 'img/dardjeeling-limited.jpg'
-  },
-  {
-    title: 'Orlando',
-    imgLink: 'img/orlando.jpg'
-  },
-  {
-    title: 'Mindhunter',
-    imgLink: 'img/mindhunter.jpg'
-  },
-  {
-    title: 'Midnight Special',
-    imgLink: 'img/midnight-special.jpg'
-  },
-];
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {Film} from '../../pages/Film';
+import {AddReview} from '../../pages/AddReview';
+import {MyList} from '../../pages/MyList';
+import {Player} from '../../pages/Player';
+import {SignIn} from '../../pages/SignIn';
 
 function App(): JSX.Element {
   return (
-    <div>
-      <Main
-        title={'The Grand Budapest Hotel'}
-        genre={'Drama'}
-        yearOfPromoRelease={2014}
-        filmsListProps={filmsListProps}
-      />
-    </div>);
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Main
+              title={'The Grand Budapest Hotel'}
+              genre={'Drama'}
+              yearOfPromoRelease={2014}
+              filmsListProps={window.FilmsList}
+            />
+          }
+        />
+        <Route
+          path='/login'
+          element={<SignIn/>}
+        />
+        <Route
+          path='/films/:id'
+        >
+          <Route index element={<Film/>} />
+          <Route path={'review'} element={<AddReview/>} />
+        </Route>
+        <Route
+          path='/mylist'
+          element={
+            <MyList
+              films={window.FilmsList.slice(0, 9)}
+              user={{ isAuthorize: false }}
+            />
+          }
+        />
+        <Route path='/player/:id' element={<Player/>}/>
+        <Route
+          path='*'
+          element={
+            <Fragment>
+              <h1>404 Page Not Found</h1>
+              <h2><Link to='/'>Main</Link></h2>
+            </Fragment>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
